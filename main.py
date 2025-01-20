fc2a9d852c60beb76e37277c723a5281f3e584a2
abhigyan-709 <abhigkumar709@gmail.com>
2025-01-21 01:15:14 +0530

    cli model ready to test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from google import genai
 import os
 from dotenv import load_dotenv
-from google.genai import types
+import speech_recognition as sr
 
 # Load environment variables from .env file
 load_dotenv()
@@ -11,5 +11,20 @@ api_key = os.getenv("API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(model='gemini-2.0-flash-exp', contents='How does AI work?')
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Hey! What do you want to know?")
+    audio = r.listen(source)
+
+try:
+    text = r.recognize_google(audio)
+    print("Google Speech Regognition thinks you asked: " + text)
+
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+response = client.models.generate_content(model='gemini-2.0-flash-exp', contents=text)
 print(response.text)
